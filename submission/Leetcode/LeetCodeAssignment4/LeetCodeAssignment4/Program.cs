89355c8490aad37d0bdbee9b63930d3bbc6c9afb
Omar Salem <omarsalemabdelrouf@gmail.com>
2026-09-24 09:17:11 +0300

    add part 25

---
+++ submission/Leetcode/LeetCodeAssignment4/LeetCodeAssignment4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeAssignment4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine( IsAnagram("anagram", "nagaram"));
+      
+    }
+    
+    public static bool IsAnagram(string s, string t)
+    {
+        char[] arrayS = s.ToCharArray();
+        char[] arrayt = t.ToCharArray();
+        Array.Sort(arrayS);
+        Array.Sort(arrayt); 
+        
+        return arrayS.SequenceEqual(arrayt);
+        
+    }
+}
